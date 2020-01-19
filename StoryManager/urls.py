4654d7c57f82e91e1416103b2090c5c28311dac5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-19 13:05:46 -0300

    added urls file to all apps

--- StoryManager/urls.py
+++ StoryManager/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('characters/', include("Characters.urls")),
+    path('items/', include("Items.urls")),
+    path('stories/', include("Stories.urls")),
+    path('places/', include("Places.urls")),
 ]
+
