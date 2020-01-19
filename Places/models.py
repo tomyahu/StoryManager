4013a22c96dc4a547b37a0f32a15769a31f61074
BuildTools <unconfigured@null.spigotmc.org>
2020-01-19 12:57:40 -0300

    added places data models

---
+++ Places/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+    story = models.TextField(max_length=10000, default="", blank=True)
+    image = models.ImageField(blank=False, upload_to='images/region_images')
+
+    def __str__(self):
+        return self.name
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    story = models.TextField(max_length=10000, default="", blank=True)
+    foundation = models.IntegerField(blank=True, null=True)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.PROTECT, blank=False, null=False)
+    image = models.ImageField(blank=False, upload_to='images/place_images')
