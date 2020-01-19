5258ddcd27eb1a57d6831f5ebc325df431d73563
BuildTools <unconfigured@null.spigotmc.org>
2020-01-19 18:21:18 -0300

    added character view

--- Characters/urls.py
+++ Characters/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-#from Characters.views import
+from Characters.views import IndexView, CharacterView
 
 urlpatterns = [
-#    path('<int:pk>', OverviewView.as_view()),
+    path('', IndexView.as_view()),
+    path('<int:pk>', CharacterView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Characters/views.py
+++ Characters/views.py
@@ -1,3 +1,24 @@
+from StoryManager.consts import PROJECT_NAME
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+from Characters.models import Character, CharacterImage
+
+class IndexView(TemplateView):
+    template_name = "character_overview.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project_name"] = PROJECT_NAME
+        context["characters"] = Character.objects.all()
+        return context
+
+class CharacterView(TemplateView):
+    template_name = "character.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project_name"] = PROJECT_NAME
+        context["character"] = Character.objects.get(id=kwargs["pk"])
+        context["character_images"] = CharacterImage.objects.filter(character=context["character"])
+        return context
