a79b378d18c8470ca58308b87fd11dea2d0a897f
Tomás Ahumada <tomyahu@gmail.com>
2020-02-12 19:58:48 -0300

    ordered stories by date

--- Stories/views.py
+++ Stories/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
     template_name = "stories_overview.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["project_name"] = PROJECT_NAME
-        context["stories"] = Story.objects.all()
+        context["stories"] = Story.objects.all().order_by('date')
         return context
+
 
 class StoryView(TemplateView):
     template_name = "story.html"
